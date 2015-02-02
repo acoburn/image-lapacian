3824a99601744a8267ce5b8e1557bdea776cbcbd
Aaron Coburn <acoburn@amherst.edu>
2015-02-02 13:08:26 -0500

    removed dead code

--- include/laplacian/laplacian.h
+++
@@ -1,7 +0,0 @@
-// <Copyright Holder>. Copyright (C) <Copyright Year(s)>. <License>
-#ifndef HEADER_LAPLACIAN_LAPLACIAN_H_INCLUDED
-#define HEADER_LAPLACIAN_LAPLACIAN_H_INCLUDED
-
-#include <laplacian/version.h>
-
-#endif
